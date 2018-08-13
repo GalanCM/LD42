212202025f2b52eb7c8472f9cd10373fe442adf7
Galan Montgomery <galan.c.m@gmail.com>
2018-08-13 13:23:39 -0400

    Esc to quit at any time.

--- game/Erased.gd
+++ game/Erased.gd
@@ -7,8 +7,6 @@ func _ready():
 	set_process_input(true)
 	
 func _input(event):
-	if event.is_action_pressed("Exit"):
-		get_tree().quit()
-	elif event.is_action_pressed("Fire"):
+	if event.is_action_pressed("Fire"):
 		get_tree().reload_current_scene()
 		queue_free()
--- game/Game.gd
+++ game/Game.gd
@@ -11,3 +11,7 @@ func get_unique_node(name):
 		if len(group) > 1:
 			print("WARNING: More than one unique node " + name + "." )
 		return group[0]
+		
+func _input(event):
+	if event.is_action_pressed("Exit"):
+		get_tree().quit()
